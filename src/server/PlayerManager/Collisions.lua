438b7cb6ca5a242ed6190bde1d64a3d1ec994e1e
pluviolithic <e-mckanna@onu.edu>
2022-10-29 16:53:54 -0400

    Disabled player collisions

---
+++ src/server/PlayerManager/Collisions.lua
@@ -0,0 +1,28 @@
+local Players = game:GetService("Players")
+local PhysicsService = game:GetService("PhysicsService")
+
+PhysicsService:CreateCollisionGroup("PlayerCharacters")
+PhysicsService:CollisionGroupSetCollidable("PlayerCharacters", "PlayerCharacters", false)
+
+local function disableCharacterCollisions(character)
+    for _, part in ipairs(character:GetDescendants()) do
+        if part:IsA("BasePart") then
+            PhysicsService:SetPartCollisionGroup(part, "PlayerCharacters")
+        end
+    end
+end
+
+local function disablePlayerCollisions(player)
+    player.CharacterAdded:Connect(disableCharacterCollisions)
+end
+
+Players.PlayerAdded:Connect(disablePlayerCollisions)
+
+for _, player in ipairs(Players:GetPlayers()) do
+    disablePlayerCollisions(player)
+    if player.Character then
+        disableCharacterCollisions(player.Character)
+    end
+end
+
+return 0
