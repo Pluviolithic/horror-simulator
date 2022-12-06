77c13571b4ba90e4d0ab11d44d26bd40d4ef2480
pluviolithic <e-mckanna@onu.edu>
2022-12-06 03:47:56 -0500

    Update healthbar to lerp color

--- src/shared/Utils/HealthBar.lua
+++ src/shared/Utils/HealthBar.lua
@@ -1,6 +1,9 @@
 local HealthBar = {}
 HealthBar.__index = HealthBar
 
+local robloxHealthGreen = Color3.fromRGB(0, 255, 17)
+local robloxHealthRed = Color3.fromRGB(255, 0, 0)
+
 function HealthBar.new(bar)
 	local self = setmetatable({}, HealthBar)
 
@@ -18,26 +21,32 @@ function HealthBar:updateText(health, maxHealth)
 	self._healthBarText.Text = string.format("%d / %d", health, maxHealth)
 end
 
+function HealthBar:recolor(health, maxHealth)
+	self._healthBar.ImageColor3 = robloxHealthRed:Lerp(robloxHealthGreen, health / maxHealth)
+end
+
+function HealthBar:update(health, maxHealth)
+	self:resize(health, maxHealth)
+	self:updateText(health, maxHealth)
+	self:recolor(health, maxHealth)
+end
+
 function HealthBar:connect(humanoid)
 	if self._healthBarConnection then
 		self._healthBarConnection:Disconnect()
 	end
 	if humanoid.ClassName == "Humanoid" then
 		self._healthBarConnection = humanoid:GetPropertyChangedSignal("Health"):Connect(function()
-			self:resize(humanoid.Health, humanoid.MaxHealth)
-			self:updateText(humanoid.Health, humanoid.MaxHealth)
+			self:update(humanoid.Health, humanoid.MaxHealth)
 		end)
-		self:resize(humanoid.Health, humanoid.MaxHealth)
-		self:updateText(humanoid.Health, humanoid.MaxHealth)
+		self:update(humanoid.Health, humanoid.MaxHealth)
 	else
 		local humanoidInstance = humanoid:FindFirstChildOfClass "Humanoid"
 		local healthValue = humanoid.Configuration.Health
 		self._healthBarConnection = healthValue:GetPropertyChangedSignal("Value"):Connect(function()
-			self:resize(healthValue.Value, humanoidInstance.MaxHealth)
-			self:updateText(healthValue.Value, humanoidInstance.MaxHealth)
+			self:update(healthValue.Value, humanoidInstance.MaxHealth)
 		end)
-		self:resize(healthValue.Value, humanoidInstance.MaxHealth)
-		self:updateText(healthValue.Value, humanoidInstance.MaxHealth)
+		self:update(healthValue.Value, humanoidInstance.MaxHealth)
 	end
 end
 
