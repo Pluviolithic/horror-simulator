37e6aee4a06b5933a32ca96edbaea5cc10607402
pluviolithic <e-mckanna@onu.edu>
2022-12-16 18:05:48 -0500

    Remove unnecessary output

--- src/server/PurchaseManager/DeveloperProducts/Gamepasses/init.lua
+++ src/server/PurchaseManager/DeveloperProducts/Gamepasses/init.lua
@@ -1,7 +1,6 @@
 local rewarders = require(script.Rewarders)
 
 return function(player, gamepassID)
-	print("rewarders[gamepassID] is: " .. tostring(rewarders[gamepassID]))
 	if not rewarders[gamepassID] then
 		return
 	end
