16430eacba726bf334297aa7029342240cc375a0
Pluviolithic <94190216+Pluviolithic@users.noreply.github.com>
2023-10-22 23:25:22 -0400

    Change multiplier admin command

--- src/server/Commands/Players/ChangeMultiplier.lua
+++ src/server/Commands/Players/ChangeMultiplier.lua
@@ -1,23 +1,23 @@
 return {
-	Name = "setmultiplier",
-	Aliases = { "sm" },
+	Name = "changemultiplier",
+	Aliases = { "cm" },
 	Description = "Sets the multiplier for a stat of a player.",
 	Group = "DefaultAdmin",
 	Args = {
 		{
 			Type = "player",
 			Name = "Player",
-			Description = "The player whose multiplier you want to set.",
+			Description = "The player whose multiplier you want to change.",
 		},
 		{
 			Type = "string",
 			Name = "Stat",
-			Description = "The stat for which you want to set the multiplier.",
+			Description = "The stat for which you want to change the multiplier.",
 		},
 		{
 			Type = "number",
 			Name = "Value",
-			Description = "The value to set the multiplier to.",
+			Description = "The value to change the multiplier to.",
 		},
 	},
 }
