ff676961ad27c88fac171e537012429e04d0a344
Pluviolithic <94190216+Pluviolithic@users.noreply.github.com>
2023-10-25 20:06:19 -0400

    Fix incorrect indexing in MultiplierData

--- src/shared/State/Reducer/MultiplierData.lua
+++ src/shared/State/Reducer/MultiplierData.lua
@@ -26,8 +26,8 @@ return Rodux.createReducer({}, {
 	end,
 	incrementPlayerMultiplier = function(state, action)
 		return produce(state, function(draft)
-			if not draft[action.playerName[action.multiplierName]] then
-				draft[action.playerName[action.multiplierName]] = 0
+			if not draft[action.playerName][action.multiplierName] then
+				draft[action.playerName][action.multiplierName] = 0
 			end
 			local multiplierWholePartCount = draft[action.playerName][action.multiplierName .. "Count"] or 0
 			draft[action.playerName][action.multiplierName] += action.incrementAmount
