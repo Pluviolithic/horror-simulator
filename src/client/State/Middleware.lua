58a218f64722a9c984c018a29d22f3aa4c6a0c92
pluviolithic <e-mckanna@onu.edu>
2023-11-08 11:42:03 -0500

    Fixes #48 and Fixes #59

--- src/client/State/Middleware.lua
+++ src/client/State/Middleware.lua
@@ -24,11 +24,22 @@ local function updateIdleAnimationMiddleware(nextDispatch, store)
 					originalAnimationId = player.Character.Animate.idle.Animation1.AnimationId
 				end
 				local equippedWeapon = selectors.getEquippedWeapon(store:getState(), player.Name)
-				if equippedWeapon then
+				if equippedWeapon and not action.currentPunchingBag then
 					player.Character.Animate.idle.Animation1.AnimationId =
 						ReplicatedStorage.CombatAnimations[equippedWeapon].Idle.AnimationId
+				else
+					player.Character.Animate.idle.Animation1.AnimationId =
+						ReplicatedStorage.CombatAnimations.Fists.Idle.AnimationId
 				end
 			elseif originalAnimationId then
+				local humanoid = player.Character and player.Character:FindFirstChild "Humanoid"
+				if humanoid then
+					for _, animationTrack in player.Character.Humanoid.Animator:GetPlayingAnimationTracks() do
+						if animationTrack.Priority == Enum.AnimationPriority.Idle then
+							animationTrack:Stop()
+						end
+					end
+				end
 				player.Character.Animate.idle.Animation1.AnimationId = originalAnimationId
 			end
 		end
