615167f5956db9203803aecccf1600f81a024c94
pluviolithic <e-mckanna@onu.edu>
2023-11-25 02:15:10 -0500

    Label admin IDs

--- src/shared/PermissionList.lua
+++ src/shared/PermissionList.lua
@@ -1,8 +1,8 @@
 return {
 	Admins = {
-		[473739220] = true,
-		[493434533] = true,
-		[112813111] = true,
-		[45834633] = true,
+		[473739220] = true, -- Prodigy_Alt
+		[493434533] = true, -- AetherProgrammer
+		[112813111] = true, -- Ex_Alt
+		[45834633] = true, -- Ex_L
 	},
 }
